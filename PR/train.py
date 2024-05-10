4dd23db4f3ae5b5f4174367a3ac8120b67443b25
Enrique <lopezyouhei@gmail.com>
2024-05-10 20:00:49 +0200

    adapt amount of epochs based on reduce factor add argparser for number of training runs

--- PR/train.py
+++ PR/train.py
@@ -1,6 +1,6 @@
+import argparse
 from lightly.loss import NTXentLoss
 from lightly.models.modules import NNMemoryBankModule
-import math
 import os
 import torch
 from torch.utils.data import DataLoader
@@ -19,7 +19,10 @@ labels_path = os.path.join(DATA_PATH, "train-labels.th")
 def train(tags, random_views=True, reduce_factor=1.0):
     with wandb.init(project="NNCLR", tags=tags) as run:
         config = wandb.config
-        config.epochs = config.get("epochs", EPOCHS)
+
+        config.random_views = config.get("random_views", random_views)
+        config.reduce_factor = config.get("reduce_factor", reduce_factor)
+        config.epochs = config.get("epochs", round(EPOCHS/config.reduce_factor))
         config.batch_size = config.get("batch_size", BATCH_SIZE)
         config.lr = config.get("lr", LR)
         config.temperature = config.get("temperature", TEMPERATURE)
@@ -29,12 +32,9 @@ def train(tags, random_views=True, reduce_factor=1.0):
         config.project_output_dim = config.get("project_output_dim", PROJECT_OUTPUT_DIM)
         config.prediction_hidden_dim = config.get("prediction_hidden_dim", PREDICTION_HIDDEN_DIM)
         config.prediction_output_dim = config.get("prediction_output_dim", PREDICTION_OUTPUT_DIM)
-        config.random_views = config.get("random_views", random_views)
-        config.reduce_factor = config.get("reduce_factor", reduce_factor)
         config.device = config.get("device", "cuda" if torch.cuda.is_available() else "cpu")
 
         # create a dataset
-        # TODO: write function which takes boolean to generate random pairs or return a given pair
         dataset = None
         view1_path, view2_path = generate_pair(config.random_views, 
                                                DATA_PATH, 
@@ -94,3 +94,13 @@ def train(tags, random_views=True, reduce_factor=1.0):
                              f"{view1_name}_{view2_name}_{epoch}_{config.reduce_factor}.pth"))
         torch.cuda.empty_cache()
         wandb.finish()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run training multiple times")
+    parser.add_argument("--runs", type=int, default=1, help="Number of times to run training")
+    args = parser.parse_args()
+
+    tags = ["baseline"]
+
+    for _ in range(args.runs):
+        train(tags, random_views=True, reduce_factor=1.0)

7b7218aae60fbe68d5cb966d268bc6d154a75ed2
Enrique <lopezyouhei@gmail.com>
2024-05-10 18:52:08 +0200

    add dictionary for class grouping

---
+++ PR/config/class_group.py
@@ -0,0 +1,7 @@
+categories = {
+    'animals': 'animal.n.01',   
+    'vehicles': 'vehicle.n.01', 
+    'household': 'artifact.n.01',
+    'food': 'food.n.01',
+    'nature': 'natural_object.n.01'
+}

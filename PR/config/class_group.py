fc1e631938a38a379c33df76edff7bb35516f4db
Enrique <lopezyouhei@gmail.com>
2024-06-06 15:48:30 +0200

    include 2 new categories

--- PR/config/class_group.py
+++ PR/config/class_group.py
@@ -1,7 +1,38 @@
-categories = {
+main_5_categories = {
     'animals': 'animal.n.01',   
     'vehicles': 'vehicle.n.01', 
     'household': 'artifact.n.01',
     'food': 'food.n.01',
     'nature': 'natural_object.n.01'
+}
+
+dog_15_categories = {
+    'maltese dog': 'maltese_dog.n.01',
+    'blenheim spaniel': 'blenheim_spaniel.n.01',
+    'basset': 'basset.n.01',
+    'norwegian elkhound': 'norwegian_elkhound.n.01',
+    'giant schnauzer': 'giant_schnauzer.n.01',
+    'golden retriever': 'golden_retriever.n.01',
+    'brittany spaniel': 'brittany_spaniel.n.01',
+    'clumber': 'clumber.n.01',
+    'welsh springer spaniel': 'welsh_springer_spaniel.n.01',
+    'groenendael': 'groenendael.n.01',
+    'kelpie': 'kelpie.n.01',
+    'shetland sheepdog': 'shetland_sheepdog.n.01',
+    'doberman': 'doberman.n.01',
+    'pug': 'pug.n.01',
+    'chow': 'chow.n.01',
+}
+
+wen_10_categories = {
+    'bird': 'bird.n.01',
+    'boat': 'boat.n.01',
+    'car': 'car.n.01',
+    'cat': 'cat.n.01',
+    'dog': 'dog.n.01',
+    'fruit': 'fruit.n.01',
+    'fungus': 'fungus.n.01',
+    'insect': 'insect.n.01',
+    'monkey': 'monkey.n.01',
+    'truck': 'truck.n.01',
 }

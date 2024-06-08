605333348273eaf14ec37a054e5415f56cbaf63c
Enrique <lopezyouhei@gmail.com>
2024-06-08 07:25:31 +0200

    change file name to knn_main.py to be consistent with other files in the PR/metrics directory

--- knn_k_sweep.ps1
+++ knn_k_sweep.ps1
@@ -3,5 +3,5 @@ $end = 9
 $step = 1
 
 for ($n_neighbors = $start; $n_neighbors -le $end; $n_neighbors += $step) {
-    python .\PR\metrics\knn.py --n_neighbors $n_neighbors
+    python .\PR\metrics\knn_main.py --n_neighbors $n_neighbors
 }

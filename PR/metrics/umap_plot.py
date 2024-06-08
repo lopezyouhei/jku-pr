79c2437f05b0e92a86590d8a843d3acf8f01641a
Enrique <lopezyouhei@gmail.com>
2024-06-08 08:53:42 +0200

    results of umap and umap_plot.py update for label display and color map

--- PR/metrics/umap_plot.py
+++ PR/metrics/umap_plot.py
@@ -16,7 +16,8 @@ def plot_umap(embedding : np.ndarray,
     fig, ax = plt.subplots()
     unique_labels = np.unique(category_labels)
 
-    colors = plt.cm.get_cmap('Dark2', len(unique_labels))
+    # colors = plt.cm.get_cmap('gist_ncar', len(unique_labels))
+    colors = plt.cm.get_cmap('tab20', 20)
 
     for i, label in enumerate(unique_labels):
         ax.scatter(embedding[:, 0][category_labels == label], 
@@ -25,7 +26,9 @@ def plot_umap(embedding : np.ndarray,
                    label=f'{category_names[int(label)].capitalize()}', 
                    s = 5)
 
-    ax.legend(title="Labels")
+    # ax.legend(title="Labels")
+    ax.legend(title="Labels", loc='upper center', bbox_to_anchor=(0.5, -0.15), 
+              fancybox=True, shadow=True, ncol=len(unique_labels)//2)
     filename = os.path.join(results_path, f"{model_name}_layer-{layer_name}_{synset_category_name}.png")
     plt.savefig(filename, dpi=300, bbox_inches='tight')
     plt.close(fig)
